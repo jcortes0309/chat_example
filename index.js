47656529ecf488501403eae6c2ae6d14c2261274
Juan A Cortes <juan.cortes@gmail.com>
2016-12-08 15:03:08 -0500

    Add ability to broadcast a message to connected users when someone connects or disconnects

--- index.js
+++ index.js
@@ -10,13 +10,25 @@ app.get("/", function(request, response) {
 });
 
 io.on("connection", function(socket) {
+  var connected = "A user joined the room";
+  var disconnected = "A user left the room";
+  // Sends the message to everyone but the socket that caused this
+  socket.broadcast.emit("chat message", connected);
   // console.log("a user connected");
   socket.on("chat message", function(msg) {
     io.emit("chat message", msg);
     console.log("message: " + msg);
   });
+  socket.on('disconnect', function() {
+    socket.broadcast.emit("chat message", disconnected);
+  });
 });
 
+
+
+// io.on("connection", function(socket) {
+//   io.emit("")
+// });
 
 
 
