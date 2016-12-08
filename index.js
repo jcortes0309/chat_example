b7e4d99d020db1fa6547e65931c60ce6b8a7ff6b
Juan A Cortes <juan.cortes@gmail.com>
2016-12-08 14:10:08 -0500

    Add ability to broadcast messages

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.get("/", function(request, response) {
 });
 
 io.on("connection", function(socket) {
+  // console.log("a user connected");
   socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
     console.log("message: " + msg);
   });
 });
