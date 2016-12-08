a4caa74e340e861d00b741cf97aa61869031ebdd
Juan A Cortes <juan.cortes@gmail.com>
2016-12-08 14:03:17 -0500

    Add ability to emmit events

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ app.get("/", function(request, response) {
 });
 
 io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("disconnect", function() {
-    console.log("user disconnected");
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
   });
 });
 
